7e019e5f1dc36bee5ba941c2a9789d2afa0c8cdc
ferueda <fe.rueda@outlook.com>
2020-07-03 19:51:25 -0400

    api requests that returns the stories, users and comments from hacker news

--- src/services/api.js
+++ src/services/api.js
@@ -17,3 +17,31 @@ const onlyComments = data => {
 const onlyStories = data => {
 	return data.filter(({ type }) => type === 'story');
 };
+
+export const getStoriesIds = async queryString => {
+	const res = await fetch(`${BASE_URL}/${queryString}.json`);
+	const data = await res.json();
+	return data;
+};
+
+export const getItemById = async id => {
+	const res = await fetch(`${BASE_URL}/item/${id}.json`);
+	const data = await res.json();
+	return data;
+};
+
+export const getUserById = async id => {
+	const res = await fetch(`${BASE_URL}/user/${id}.json`);
+	const data = await res.json();
+	return data;
+};
+
+export const getStories = async ids => {
+	const stories = await Promise.all(ids.slice(0, 50).map(id => getItemById(id)));
+	return cleanData(onlyStories(stories));
+};
+
+export const getComments = async ids => {
+	const comments = await Promise.all(ids.map(id => getItemById(id)));
+	return cleanData(onlyComments(comments));
+};
