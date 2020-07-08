6f5bc36925e7f3ce0a5fb4e421b2727e846c3850
ferueda <fe.rueda@outlook.com>
2020-07-08 18:35:10 -0400

    refactor Story.js refactored into functional component

--- src/components/Story.js
+++ src/components/Story.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useReducer, useState } from 'react';
 import queryString from 'query-string';
 
 import { getItemById, getComments } from '../services/api';
@@ -7,50 +7,65 @@ import Loading from './Loading';
 import StoryMeta from './StoryMeta';
 import Comments from './Comments';
 
-class Story extends React.Component {
-	constructor(props) {
-		super(props);
-
-		this.state = {
-			story: null,
-			isLoading: true,
-			isError: false,
-			comments: [],
-		};
+const storyReducer = (state, action) => {
+	switch (action.type) {
+		case 'SUCCESS':
+			return {
+				...state,
+				comments: [...action.payload],
+				isLoading: false,
+				isError: false,
+			};
+		case 'ERROR':
+			return {
+				...state,
+				isError: true,
+				isLoading: false,
+			};
+		default:
+			return { ...state };
 	}
+};
 
-	componentDidMount() {
-		const { id } = queryString.parse(this.props.location.search);
+const Story = ({ location }) => {
+	const [story, setStory] = useState(null);
+
+	const [{ comments, isLoading, isError }, dispatch] = useReducer(storyReducer, {
+		isLoading: true,
+		isError: false,
+		comments: [],
+	});
+
+	const { id } = queryString.parse(location.search);
+
+	useEffect(() => {
 		getItemById(id)
-			.then(res => {
-				this.setState({ isLoading: false, story: res });
-				return getComments(res.kids || []);
+			.then(data => {
+				setStory(data);
+				return getComments(data.kids || []);
 			})
-			.then(res => this.setState({ comments: res }))
-			.catch(() => this.setState({ isLoading: true, isError: true }));
-	}
+			.then(data => dispatch({ type: 'SUCCESS', payload: data }))
+			.catch(() => dispatch({ type: 'ERROR' }));
+	}, [id]);
 
-	render() {
-		const { story, comments, isLoading, isError } = this.state;
-		return (
-			<React.Fragment>
-				{isLoading ? (
-					<Loading message="Loading" speed={300} />
-				) : (
-					<React.Fragment>
-						<h2>
-							<a className="main-link" href={story.url}>
-								{story.title}
-							</a>
-						</h2>
-						<StoryMeta id={story.id} by={story.by} time={story.time} kids={story.kids} />
-						<Comments comments={comments} />
-					</React.Fragment>
-				)}
-				{isError && <h2 className="text-center">An error ocurred</h2>}
-			</React.Fragment>
-		);
-	}
-}
+	return (
+		<React.Fragment>
+			{isLoading ? (
+				<Loading message="Loading" speed={300} />
+			) : (
+				<React.Fragment>
+					<h2>
+						<a className="main-link" href={story.url}>
+							{story.title}
+						</a>
+					</h2>
+					<StoryMeta id={story.id} by={story.by} time={story.time} kids={story.kids} />
+					<Comments comments={comments} />
+				</React.Fragment>
+			)}
+			{isError && <h2 className="text-center">An error ocurred</h2>}
+		</React.Fragment>
+	);
+};
 
 export default Story;
