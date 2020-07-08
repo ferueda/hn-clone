4bb4e34ca6cab5ef1968b92ad7b3b82871657ce2
ferueda <fe.rueda@outlook.com>
2020-07-08 18:35:29 -0400

    refactor StoryPage.js refactored into functional component

--- src/components/StoryPage.js
+++ src/components/StoryPage.js
@@ -1,64 +1,64 @@
-import React from 'react';
-import { getStoriesIds, getStories, getUserById, getItemById, getComments, queryStrings } from '../services/api';
+import React, { useReducer, useEffect } from 'react';
+
+// import { getStoriesIds, getStories, queryStrings } from '../services/api';
+import { getStories } from '../services/swrApi';
+
+import useStoriesId from '../hooks/useStoriesId';
 
 import StoryList from './StoryList';
 import Loading from './Loading';
 
-class StoryPage extends React.Component {
-	constructor(props) {
-		super(props);
-
-		this.state = {
-			stories: [],
-			isLoading: true,
-			isError: false,
-		};
+const storyPageReducer = (state, action) => {
+	switch (action.type) {
+		case 'FETCH_INIT':
+			return {
+				...state,
+				stories: [],
+				isLoading: true,
+				isError: false,
+			};
+		case 'FETCH_SUCCESS':
+			return {
+				...state,
+				stories: [...action.payload],
+				isLoading: false,
+				isError: false,
+			};
+		case 'FETCH_ERROR':
+			return {
+				...state,
+				isLoading: false,
+				isError: true,
+			};
+		default:
+			return { ...state };
 	}
+};
 
-	componentDidMount() {
-		this.fetchStories();
-	}
+const StoryPage = ({ type }) => {
+	const { ids } = useStoriesId(type);
 
-	componentDidUpdate(prevProps) {
-		if (prevProps.type !== this.props.type) {
-			this.fetchStories();
-		}
-	}
+	const [{ stories, isLoading, isError }, dispatch] = useReducer(storyPageReducer, {
+		stories: [],
+		isLoading: true,
+		isError: false,
+	});
 
-	fetchStories() {
-		this.setState({
-			isLoading: true,
-			isError: false,
-			stories: [],
-		});
+	useEffect(() => {
+		dispatch({ type: 'FETCH_INIT' });
 
-		getStoriesIds(queryStrings[this.props.type])
-			.then(ids => getStories(ids))
-			.then(res => {
-				this.setState({
-					stories: res,
-					isLoading: false,
-					isError: false,
-				});
-			})
-			.catch(() => {
-				this.setState({
-					isError: true,
-					isLoading: false,
-				});
-			});
-	}
+		getStories(ids || [])
+			.then(stories => dispatch({ type: 'FETCH_SUCCESS', payload: stories }))
+			.catch(() => dispatch({ type: 'FETCH_ERROR' }));
+	}, [ids]);
 
-	render() {
-		const { stories, isLoading, isError } = this.state;
-		return (
-			<React.Fragment>
-				<StoryList stories={stories} />
-				{isLoading && <Loading message="Loading" speed={300} />}
-				{isError && <h2 className="text-center">An error ocurred... Please refresh</h2>}
-			</React.Fragment>
-		);
-	}
-}
+	return (
+		<React.Fragment>
+			<StoryList stories={stories} />
+			{isLoading && <Loading message="Loading" speed={300} />}
+			{isError && <h2 className="text-center">An error ocurred... Please refresh</h2>}
+		</React.Fragment>
+	);
+};
 
 export default StoryPage;
