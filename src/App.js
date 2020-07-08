032c3fa970fbb0b039fdd7ef63efff78918f115a
ferueda <fe.rueda@outlook.com>
2020-07-08 18:33:20 -0400

    refactor App.js refactored into functional component

--- src/App.js
+++ src/App.js
@@ -1,48 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import './App.css';
+import ThemeContext from './context/ThemeContext';
 
 import Nav from './components/Nav';
 import StoryPage from './components/StoryPage';
 import User from './components/User';
 import Story from './components/Story';
 
-class App extends React.Component {
-	constructor(props) {
-		super(props);
+const App = () => {
+	const [theme, setTheme] = useState('light');
 
-		this.state = {
-			theme: 'light',
-			toggleTheme: () => {
-				this.setState(({ theme }) => ({
-					theme: theme === 'light' ? 'dark' : 'light',
-				}));
-			},
-		};
-	}
+	const toggleTheme = () => setTheme(theme => (theme === 'light' ? 'dark' : 'light'));
 
-	render() {
-		const { theme, toggleTheme } = this.state;
-		return (
-			<Router>
-				<main className={`container`}>
-					<Nav theme={theme} toggleTheme={toggleTheme} />
+	return (
+		<Router>
+			<ThemeContext.Provider value={theme}>
+				<main className={theme}>
+					<div className="container">
+						<Nav toggleTheme={toggleTheme} />
 
-					<Switch>
-						<Route exact path="/" render={() => <StoryPage type="top" />} />
-						<Route exact path="/best" render={() => <StoryPage type="best" />} />
-						<Route exact path="/new" render={() => <StoryPage type="new" />} />
+						<Switch>
+							<Route exact path="/" render={() => <StoryPage type="top" />} />
+							<Route exact path="/best" render={() => <StoryPage type="best" />} />
+							<Route exact path="/new" render={() => <StoryPage type="new" />} />
 
-						<Route path="/user" component={User} />
-						<Route path="/story" component={Story} />
+							<Route path="/user" component={User} />
+							<Route path="/story" component={Story} />
 
-						<Route render={() => <h2 className="text-center">Page not found</h2>} />
-					</Switch>
+							<Route render={() => <h2 className="text-center">Page not found</h2>} />
+						</Switch>
+					</div>
 				</main>
-			</Router>
-		);
-	}
-}
+			</ThemeContext.Provider>
+		</Router>
+	);
+};
 
 export default App;
